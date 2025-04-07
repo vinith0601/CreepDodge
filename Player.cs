b2c73058fd0b961c7df666d91888d7315a30b2ed
Vinith Rajaretnam <Vinith0601@gmail.com>
2025-04-07 19:04:41 -0400

    Added player scene

---
+++ Player.cs
@@ -0,0 +1,81 @@
+using Godot;
+using System;
+
+public partial class Player : Area2D
+{
+	[Signal]
+	public delegate void HitEventHandler();
+
+	[Export]
+	public int Speed = 400;
+
+	public Vector2 ScreenSize;
+
+
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+	}
+
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero;
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
+
+		var animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		if (velocity.Length()> 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite.Play();
+
+		}
+		else
+		{
+			animatedSprite.Stop();
+		}
+
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+		if (velocity.X != 0)
+		{
+			animatedSprite.Animation = "walk";
+			animatedSprite.FlipV = false;
+			animatedSprite.FlipH = velocity.X < 0;
+		} else if (velocity.Y != 0){
+			animatedSprite.Animation = "up";
+			animatedSprite.FlipV = velocity.Y > 0;
+		}
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		Hide();
+		EmitSignal(SignalName.Hit);
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+	}
+
+	public void Start(Vector2 position)
+	{
+		Position = position;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+	}
+}
