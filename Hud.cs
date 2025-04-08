b0a354a4c882f7ff45a056b8c5b4f58a7300db97
Vinith Rajaretnam <Vinith0601@gmail.com>
2025-04-07 20:24:35 -0400

    Finished Basic version of game

---
+++ Hud.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public partial class Hud : CanvasLayer
+{
+	[Signal]
+    public delegate void StartGameEventHandler();
+
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+	public void ShowMessage(string text)
+	{
+		var message = GetNode<Label>("Message");
+		message.Text = text;
+		message.Show();
+
+		GetNode<Timer>("MessageTimer").Start();
+	}
+
+	async public void ShowGameOver()
+	{
+		ShowMessage("Game Over");
+
+		var messageTimer = GetNode<Timer>("MessageTimer");
+		await ToSignal(messageTimer, Timer.SignalName.Timeout);
+
+		var message = GetNode<Label>("Message");
+		message.Text = "Dodge the Creeps!";
+		message.Show();
+
+		await ToSignal(GetTree().CreateTimer(1.0), SceneTreeTimer.SignalName.Timeout);
+		GetNode<Button>("StartButton").Show();
+	}
+	public void UpdateScore(int score)
+	{
+		GetNode<Label>("ScoreLabel").Text = score.ToString();
+	}
+	private void OnStartButtonPressed()
+	{
+		GetNode<Button>("StartButton").Hide();
+		EmitSignal(SignalName.StartGame);
+	}
+
+	private void OnMessageTimerTimeout()
+	{
+		GetNode<Label>("Message").Hide();
+	}
+	
+}
